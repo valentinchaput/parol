ac1fc75b4c7841032463c2cc22dea0eca617e73f
Sophia Kc <sophiakc@gmail.com>
2015-03-02 19:22:51 +0100

    changes avec Seb

--- app/models/amendment.rb
+++ app/models/amendment.rb
@@ -1,7 +1,5 @@
 class Amendment < ActiveRecord::Base
   belongs_to :law_article
-  # has_many :instructions
-  # has_many :articles, through: :instructions
 
   validates :date, presence: true
   validates :amendment_number, presence: true
--- app/models/article.rb
+++
@@ -1,7 +0,0 @@
-class Article < ActiveRecord::Base
-  belongs_to :code
-  has_many :instructions
-
-  validates :title, presence: true
-  validates :content, presence: true, uniqueness: true
-end
