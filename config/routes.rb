0747571c535045641e5edd40d8e4ac4253a706c6
Jean Hamant <jhamant@gmail.com>
2015-03-03 14:36:40 +0100

    modif routes et navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
 
   match '/about',   to: 'pages#about',   via: 'get'
 
-=======
   ActiveAdmin.routes(self)
->>>>>>> 0db9a4e9a4d31bde4fbe98a0efb8cb7cfafbbf3b
   get 'law_articles/show'
   get 'home/index'
 
