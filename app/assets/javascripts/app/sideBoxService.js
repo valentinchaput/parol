4d803227705988426496a7a896ca85cdeb1eaa89
Sophia Kc <sophiakc@gmail.com>
2015-03-03 12:40:58 +0100

    fichier apres derniere modif

--- app/assets/javascripts/app/sideBoxService.js
+++ app/assets/javascripts/app/sideBoxService.js
@@ -1,6 +1,37 @@
 $(document).ready(function() {
-  $('.amendment').on('click', function() {
-    amendmentId = $(this).data('amendmentid')
-    $('#amendment_details_' + amendmentId).toggleClass('bg-active').toggleClass('hidden')
+
+  $(document).keyup(function(e){ // pour la fonction édition d'un amendement à partir d'une zone de texte
+    if (e.keyCode == 67) {
+      var matchedText = window.getSelection().toString()
+    }
+  })
+
+  $('.amendment').on('click', function(e) { // on click sur l'amendement texte
+    e.preventDefault() // j'annule le comportement par défault au click, par exemple, au click sur un lien = ça remonte la page
+    amendmentId = $(this).data('amendmentid'); // on choppe l'id de l'amendment
+    var that = $(this) // trick js
+
+    if ($('.bg-active').length) { // si il y a déjà un amendement displayed
+      if (that.hasClass('active')) { // si on click sur le l'amendment déjà display
+        // il ne se passe rien car c'est déjà display ;)
+      } else {
+        $('.active').removeClass('active'); // sinon, on remove l'état actif des éléments actifs
+        $('.bg-active').removeClass('bg-active').addClass('hidden'); // on enlève l'amendement précédent de la box
+        getAmendment(amendmentId)
+      }
+
+    } else {
+      getAmendment(amendmentId)
+    }
+
+    function getAmendment(amendmentId) {
+      $('#amendment_details_' + amendmentId).toggleClass('bg-active').toggleClass('hidden');
+      if ($('[data-amendmentid="' + amendmentId + '"]').size() > 1) {
+        $('[data-amendmentid="' + amendmentId + '"]').toggleClass('active');
+      } else {
+        that.toggleClass('active')
+      }
+    }
   })
 })
+
