2e4f4b3fa17175169720579efe9b1b40b71de25e
Sophia Kc <sophiakc@gmail.com>
2015-03-03 11:17:46 +0100

    début avec Tchret

--- app/assets/javascripts/app/sideBoxService.js
+++ app/assets/javascripts/app/sideBoxService.js
@@ -2,5 +2,9 @@ $(document).ready(function() {
   $('.amendment').on('click', function() {
     amendmentId = $(this).data('amendmentid')
     $('#amendment_details_' + amendmentId).toggleClass('bg-active').toggleClass('hidden')
+
+    if ($('[data-amendmentid="+' amendmentId '+"]').size() > 1) {
+
+    }
   })
 })
