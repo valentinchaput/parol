9e43b15600276b860c09a940af6856a2ed251492
Amelie Beurrier <abeurrier@gmail.com>
2015-03-05 16:34:06 +0100

    page code

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,30 @@ codes = Code.create! [
     {
   title:        "Code de l'environnement",
   description:  "Code des plantes et des fleurs",
+  },
+   {
+  title:        "Code pénal",
+  description:  "Code pénal",
+  },
+    {
+  title:        "Code civil",
+  description:  "Code du mariage et des enfants",
+  },
+   {
+  title:        "Code de la recherche",
+  description:  "Code de la recherche, des bourses et de l'enseignement universitaire",
+  },
+    {
+  title:        "Code de la propriété intellectuelle",
+  description:  "PI, R&D et brevets",
+  },
+   {
+  title:        "Code du travail",
+  description:  "Métro, boulot, dodo",
+  },
+    {
+  title:        "Code de la route",
+  description:  "Feux verts, priorités et stops",
   }]
 
 Law.destroy_all
@@ -43,11 +67,35 @@ Cette refondation porte non seulement un projet éducatif, mais également un pr
 
 LawArticle.destroy_all
 law = laws[0]
+law_article1 = law.law_articles.create!(
+  article_number: "Article 1",
+  content:        "abc",
+  object:         "abc",
+  status:         "abc"
+)
 law_article = law.law_articles.create!(
   article_number: "Article 2",
   content:        "L'éducation est la première priorité nationale. Le service public de l'éducation est conçu et organisé en fonction des élèves et des étudiants. Il contribue à l'égalité des chances et à lutter contre les inégalités sociales et territoriales en matière de réussite scolaire et éducative. Il reconnaît que tous les enfants partagent la capacité d'apprendre et de progresser. Il veille à l'inclusion scolaire de tous les enfants, sans aucune distinction. Il veille également à la mixité sociale des publics scolarisés au sein des établissements d'enseignement. Pour garantir la réussite de tous, l'école se construit avec la participation des parents, quelle que soit leur origine sociale. Elle s'enrichit et se conforte par le dialogue et la coopération entre tous les acteurs de la communauté éducative.\n\nOutre la transmission des connaissances, la Nation fixe comme mission première à l'école de faire partager aux élèves les valeurs de la République. Le service public de l'éducation fait acquérir à tous les élèves le respect de l'égale dignité des êtres humains, de la liberté de conscience et de la laïcité. Par son organisation et ses méthodes, comme par la formation des maîtres qui y enseignent, il favorise la coopération entre les élèves.\n\nDans l'exercice de leurs fonctions, les personnels mettent en oeuvre ces valeurs.\n\nLe droit à l'éducation est garanti à chacun afin de lui permettre de développer sa personnalité, d'élever son niveau de formation initiale et continue, de s'insérer dans la vie sociale et professionnelle, d'exercer sa citoyenneté.\n\nPour garantir ce droit dans le respect de l'égalité des chances, des aides sont attribuées aux élèves et aux étudiants selon leurs ressources et leurs mérites. La répartition des moyens du service public de l'éducation tient compte des différences de situation, notamment en matière économique et sociale.\n\nElle a pour but de renforcer l'encadrement des élèves dans les écoles et établissements d'enseignement situés dans des zones d'environnement social défavorisé et des zones d'habitat dispersé, et de permettre de façon générale aux élèves en difficulté, quelle qu'en soit l'origine, en particulier de santé, de bénéficier d'actions de soutien individualisé.\n\nL'école garantit à tous les élèves l'apprentissage et la maîtrise de la langue française.\n\nL'acquisition d'une culture générale et d'une qualification reconnue est assurée à tous les jeunes, quelle que soit leur origine sociale, culturelle ou géographique.",
   object:         "Modifie l'article L.111-1 du Code de l'éducation",
   status:         "en discussion"
+)
+law_article3 = law.law_articles.create!(
+  article_number: "Article 3",
+  content:        "abc",
+  object:         "abc",
+  status:         "abc"
+)
+law_article4 = law.law_articles.create!(
+  article_number: "Article 4",
+  content:        "abc",
+  object:         "abc",
+  status:         "abc"
+)
+law_article5 = law.law_articles.create!(
+  article_number: "Article 5",
+  content:        "abc",
+  object:         "abc",
+  status:         "abc"
 )
 
 Amendment.destroy_all
